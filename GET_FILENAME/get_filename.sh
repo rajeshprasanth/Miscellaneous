ef598da3d8c78886f579bb718c39bbc24b35e87f
Rajesh Prashanth <rajeshprasanth@rediffmail.com>
2018-02-04 19:31:24 +0530

    stripped file extenstion

--- GET_FILENAME/get_filename.sh
+++ GET_FILENAME/get_filename.sh
@@ -3,6 +3,8 @@
 # Author: Rajesh Prashanth Anandavadivel <rajeshprasanth@rediffmail.com>
 # Date:21 23:12:46 IST 2017
 #
+# Change-log
+# Sun Feb  4 19:29:50 IST 2018       Additional feature added to strip extenstion added.
 #--------------------------------------------------------------------------------
 # Description : Get the filename in the specified path and assign it to a user 
 #               specified variable, so that data capture plugin in rundeck can 
@@ -19,7 +21,9 @@
 #--------------------------------------------------------------------------------
 
 cd $1
-filename=`ls -1v $2|head -n1`
+filename=`ls -1v *$2|head -n1`
 for last; do true; done
 variable_str=$last
 echo "$variable_str=$filename"
+FILE_BASENAME=`basename $filename $2`
+echo "FILE_BASENAME=$FILE_BASENAME"
